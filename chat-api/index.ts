9efd1380b4bf7af80d8ad9b48a11fa5cacf091d9
Khrunev Denis <deniskhrunev@gmail.com>
2024-10-06 13:34:49 +0600

    Создана структура в index для запуска сервера

---
+++ chat-api/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import config from './config';
+import * as mongoose from 'mongoose';
+import usersRouter from './routes/users';
+import chatRouter from './routes/chat';
+import expressWs from 'express-ws';
+
+const app = express();
+expressWs(app);
+const port = 8000;
+
+app.use(cors(config.corsOptions));
+app.use(express.json());
+app.use('/users', usersRouter);
+app.use('/chat', chatRouter);
+
+const run = async () => {
+  await mongoose.connect(config.database);
+
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
